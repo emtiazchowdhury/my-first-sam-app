797fa9ae0790e5a0142d99968e4c727e88cc4676
Emtiaz Chowdhury <emtiazchow@gmail.com>
2024-09-19 10:26:20 -0400

    first upload

---
+++ lambda_function/current_date.py
@@ -0,0 +1,15 @@
+import json
+from datetime import datetime
+
+
+def lambda_handler(event, context):
+    # Get the current date and time
+    current_time = datetime.now().isoformat()
+    print("Current time: "+current_time)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Here is the current date and time',
+            'current_time': current_time
+        })
+    }
