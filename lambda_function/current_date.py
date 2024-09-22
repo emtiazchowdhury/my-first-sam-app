24b119ce303ee164730d68f2cc06b7ad5c477789
Emtiaz Chowdhury <emtiazchow@gmail.com>
2024-09-21 22:25:30 -0400

    upload project

--- lambda_function/current_date.py
+++ lambda_function/current_date.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def lambda_handler(event, context):
     # Get the current date and time
     current_time = datetime.now().isoformat()
-    print("Current time: "+current_time)
+    print("Current time:"+current_time)
     return {
         'statusCode': 200,
         'body': json.dumps({
@@ -13,3 +13,6 @@ def lambda_handler(event, context):
             'current_time': current_time
         })
     }
+
+
+#lambda_handler(event='hello.json', context=1)
